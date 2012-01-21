95646a881dd0d277633f6066b68399c04c8acbdc
Andrew M <fileformat@gmail.com>
2012-01-21 14:42:02 -0500

    started real work on test.json

--- regexplanet.rb
+++ regexplanet.rb
@@ -1,20 +1,66 @@
 require 'sinatra'
 require "sinatra/json"
 
+configure do
+	mime_type :ico, 'image/x-icon'
+	set :static_cache_control, [:public, {:max_age => 604800 } ]
+end
+
+error do
+	json( {
+		:success => false,
+		:message => env['sinatra.error'].message,
+		:error_name => env['sinatra.error'].name,
+	} )
+end
+
 get '/status.json' do
 	json( {
-		:status => true,
+		:success => true,
 		:message => "OK",
+		"request.host" => request.host,
+		"request.port" => request.port,
+		:RUBY_DESCRIPTION => RUBY_DESCRIPTION,
 		:RUBY_PLATFORM => RUBY_PLATFORM,
 		:RUBY_PATCHLEVEL => RUBY_PATCHLEVEL,
+		:RUBY_RELEASE_DATE => RUBY_RELEASE_DATE,
 		:RUBY_VERSION => RUBY_VERSION,
 	} )
 end
 
 get '/test.json' do
-	"Coming soon"
+	output = ""
+
+	output << "<table>\n"
+
+	output << "\t<tr>\n"
+	output << "\t\t<td>Regular Expression</td>\n"
+	output << "\t\t<td>"
+	output << h(params[:regex])
+	output << "</td>\n"
+	output << "\t</tr>\n"
+
+	output << "\t<tr>\n"
+	output << "\t\t<td>Replacement</td>\n"
+	output << "\t\t<td>"
+	output << h(params[:replacement])
+	output << "</td>\n"
+	output << "\t</tr>\n"
+
+	output << "</table>\n"
+
+	json( {
+		:success => true,
+		:message => "OK",
+		:html => output,
+	} )
 end
 
 get '/' do
-	"Hello world!"
+	redirect 'http://www.regexplanet.com/advanced/ruby/index.html'
+end
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
 end
