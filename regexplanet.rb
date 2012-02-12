2b5c4a84041e5ac372826e23eeacac12cc383079
Andrew M <fileformat@gmail.com>
2012-02-11 20:33:58 -0500

    progress but not done yet

--- regexplanet.rb
+++ regexplanet.rb
@@ -1,5 +1,7 @@
+require 'cgi'
 require 'sinatra'
 require "sinatra/jsonp"
+require 'sinatra/reloader'
 
 configure do
 	mime_type :ico, 'image/x-icon'
@@ -31,25 +33,107 @@ end
 get '/test.json' do
 	output = ""
 
-	output << '<table class="table table-bordered table-striped" style="width:auto;">\n'
+	str_regex = params[:regex]
 
-	output << '\t<tr>\n'
-	output << '\t\t<td>Regular Expression</td>\n'
-	output << '\t\t<td>'
-	output << h(params[:regex])
-	output << '</td>\n'
-	output << '\t</tr>\n'
+	output << "<table class=\"table table-bordered table-striped\" style=\"width:auto;\">\n"
 
-	output << '\t<tr>\n'
-	output << '\t\t<td>Replacement</td>\n'
-	output << '\t\t<td>'
+	output << "\t<tr>\n"
+	output << "\t\t<td>Regular Expression</td>\n"
+	output << "\t\t<td>"
+	output << h(str_regex)
+	output << "</td>\n"
+	output << "\t</tr>\n"
+
+	output << "\t<tr>\n"
+	output << "\t\t<td>Replacement</td>\n"
+	output << "\t\t<td>"
 	output << h(params[:replacement])
-	output << '</td>\n'
-	output << '\t</tr>\n'
+	output << "</td>\n"
 
-	output << '</table>\n'
+	# when ruby 1.9
+	#if Regexp.try_convert(str_regex)
+	#	output << "\t<tr>\n"
+	#	output << "\t\t<td>Error</td>\n"
+	#	output << "\t\t<td>try_convert failed</td>\n"
+	#	output << "\t</tr>\n"
+	#	output << "</table>"
+	#	jsonp( {
+	#		:success => false,
+	#		:message => "Regexp::try_convert failed!",
+	#		:html => output,
+	#	} )
+	#end
 
+	regex = Regexp.new(str_regex)
+
+	output << "\t</tr>\n"
+
+	output << "</table>\n"
+
+	output << "<table class=\"table table-bordered table-striped\">\n"
+
+	output << "\t<tr>\n"
+	output << "\t\t<th style=\"text-align:center;\">Test</th>\n"
+	output << "\t\t<th>Input</th>"
+	output << "\t\t<th>match()</th>"
+	output << "\t</tr>\n"
+
+	inputs = request_params_multi(request.query_string)["input"]
+
+	print inputs
+
+	if not inputs
+		output << "\t<tr>\n"
+		output << "\t\t<td colspan=\"2\"><i>"
+		#output << "(no inputs)"
+		output << h(request.query_string)
+		output << "</i></td>\n"
+		output << "\t</tr>\n"
+	else
+		for index in 0..inputs.length - 1
+			input = inputs[index]
+
+			if input == nil or input.length == 0
+				next
+			end
+
+			output << "\t<tr>\n"
+			output << "\t\t<td style=\"text-align:center;\">"
+			output << (index + 1).to_s
+			output << "</td>\n"
+			output << "\t\t<td>"
+			output << h(input)
+			output << "</td>\n"
+			md = regex.match(input)
+			if md == nil
+				output << "\t\t<td><i>"
+				output << "nil"
+				output << "</i></td>\n"
+				output << "\t</tr>\n"
+			else
+				first = true
+				while md != nil
+					if first
+						first = false
+					else
+						output << "\t<tr>\n"
+						output << "\t\t<td colspan=\"2\" style=\"text-align:right;\">regex.match(matchdata.post_match)</td>"
+					end
+					output << "\t\t<td>"
+					output << h(md.inspect)
+					output << "</td>\n"
+					output << "\t</tr>\n"
+					md = regex.match(md.post_match)
+				end
+			end
+		end
+	end
+
+	output << "</table>\n"
+
+	content_type :text
 	jsonp( {
+			:debug => inputs,
 		:success => true,
 		:message => "OK",
 		:html => output,
@@ -64,3 +148,14 @@ helpers do
   include Rack::Utils
   alias_method :h, :escape_html
 end
+
+# I hope I missed something & there is a built-in way to do this...
+def request_params_multi(qs)
+	params = {}
+	qs.split('&').each do |pair|
+	  kv = pair.split('=').map{|v| CGI.unescape(v)}
+	  params.merge!({kv[0]=> kv.length > 1 ? kv[1] : nil }) {|key, o, n| o.is_a?(Array) ? o << n : [o,n]}
+	end
+	params
+end
+
